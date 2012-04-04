acae234bb96ae226c754474e2aafcde2374a3750
dggomes <dg.gomes@gmail.com>
2012-04-04 17:39:43 +0100

    londonmarathon

---
+++ LondonMarathon/branches/adhoc/LondonMarathonTests/LondonMarathonTests.h
@@ -0,0 +1,13 @@
+//
+//  LondonMarathonTests.h
+//  LondonMarathonTests
+//
+//  Created by Daniel Gomes on 04/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LondonMarathonTests : SenTestCase
+
+@end
---
+++ LondonMarathon/trunk/LondonMarathonTests/LondonMarathonTests.m
@@ -0,0 +1,32 @@
+//
+//  LondonMarathonTests.m
+//  LondonMarathonTests
+//
+//  Created by Daniel Gomes on 04/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "LondonMarathonTests.h"
+
+@implementation LondonMarathonTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LondonMarathonTests");
+}
+
+@end
